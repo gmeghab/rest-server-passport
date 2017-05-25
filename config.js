e99720d33a3a950a91fc1655a49c91b0f5bb8839
George <gmeghab@hotmail.com>
2017-05-24 20:30:24 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '815438215288023',
+        clientSecret: '95346c11a3e4ec1eba6d67b34d7cc2c9',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
+}
